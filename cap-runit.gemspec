2a315d74dcc5be024a32144ae0b87c369d3028a4
Conrad Irwin <conrad.irwin@gmail.com>
2014-03-27 00:09:46 -0700

    v0.1

---
+++ cap-runit.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |gem|
+  gem.name = 'cap-runit'
+  gem.version = '0.1'
+
+  gem.summary = 'Capistrano 3 Runit integration'
+  gem.description = "Template for managing runit directories with capistrano 3"
+
+  gem.authors = ['Conrad Irwin']
+  gem.email = %w(conrad@bugsnag.com)
+  gem.homepage = 'http://github.com/ConradIrwin/cap-runit'
+
+  gem.license = 'MIT'
+
+  gem.add_dependency 'capistrano', '>= 3.0'
+
+  gem.files = `git ls-files`.split("\n")
+end
---
+++ lib/cap-runit.rb
@@ -0,0 +1,134 @@
+#  TODO:CI make this into a gem.
+
+set :runit_service_dir, "/apps/service"
+
+class RunitServiceBuilder
+
+  def initialize(&block)
+    instance_eval(&block)
+    unless @roles
+      raise ArgumentError, "No role configured for runit service"
+    end
+    unless @run
+      raise ArgumentError, "No run script configured for runit service"
+    end
+  end
+
+  def roles(*roles)
+    @roles = roles unless roles == []
+    @roles
+  end
+
+  def run(str=nil)
+    @run = str unless str.nil?
+    @run
+  end
+
+  def log(str=nil)
+    @log = str unless str.nil?
+    @log
+  end
+
+  def finish(str=nil)
+    @finish = str unless str.nil?
+    @finish
+  end
+
+end
+
+# Defines a runit service.
+#
+# @example
+#
+#   # A runit directory that your deploy user can write to.
+#   # You can set one of these up using runsvdir.
+#
+#   set :runit_service_dir, '/etc/service'
+#
+#   # The first argument is the name of the service.
+#   # The remainder are passed to "on" to decide
+#   # which servers to use
+#   runit_service 'sidekiq', roles(:sidekiq) do
+#
+#     # required, a bash script that execs your app
+#     run <<-EOF
+#   #!/bin/bash
+#   cd /var/www/app
+#   exec bundle exec sidekiq 2>&1
+#     EOF
+#
+#     # optional, a log script that reads stdin/stdout
+#     log <<-EOF
+#   #!/bin/bash
+#   mkdir -p /var/log/app/sidekiq
+#   exec syslogd -tt /var/log/app/sidekiq
+#     EOF
+#
+#     # optional, a finish script that runs when your app quits
+#     finish <<-EOF
+#   #!/bin/bash
+#   echo 'QUIT' >> /var/log/app/sidekiq/current
+#     EOF
+#
+#   end
+#
+def runit_service(name, &block)
+  runit_service_dir = fetch(:runit_service_dir)
+
+  service = RunitServiceBuilder.new(&block)
+
+  namespace name do
+    desc "Configure #{name} runit"
+    task :configure do
+      on roles(service.roles) do
+        unless test("[ -e #{runit_service_dir}/#{name}/run ]")
+          execute <<-EOF
+          rm -rf #{runit_service_dir}/#{name}
+          mkdir -p #{runit_service_dir}/#{name}/log
+          EOF
+
+          upload! StringIO.new(service.run), "#{runit_service_dir}/#{name}/run"
+
+          if service.log
+            upload! StringIO.new(service.log), "#{runit_service_dir}/#{name}/log/run"
+          end
+
+          if service.finish
+            upload! StringIO.new(service.finish), "#{runit_service_dir}/#{name}/finish"
+          end
+
+          execute "chmod +x #{runit_service_dir}/#{name}/run #{runit_service_dir}/#{name}/log/run"
+        end
+      end
+    end
+
+    desc "Start #{name}"
+    task start: :configure do
+      on roles(*service.roles) do
+        execute "sv start #{runit_service_dir}/#{name}"
+      end
+    end
+
+    desc "Stop #{name}"
+    task :stop do
+      on roles(*service.roles) do
+        execute "sv stop #{runit_service_dir}/#{name}"
+      end
+    end
+
+    desc "Restart #{name}"
+    task restart: :configure do
+      on roles(*service.roles) do
+        execute "sv restart #{runit_service_dir}/#{name}"
+      end
+    end
+
+    desc "Disable #{name}"
+    task :disable do
+      on roles(*service.roles) do
+        execute "rm -rf #{runit_service_dir}/#{name}"
+      end
+    end
+  end
+end
+
